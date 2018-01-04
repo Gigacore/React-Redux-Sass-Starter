3dee0dd0469d0cd330777e9c5caacc927059d3c6
Gigacore <santhoshinbang@gmail.com>
2018-01-04 10:39:44 +0530

    inline documentation

--- src/actions/counterActions.js
+++ src/actions/counterActions.js
@@ -1,3 +1,10 @@
+// Actions are payloads of information that send data from your application to your store. 
+// They are the only source of information for the store.
+
+// Actions are triggered either by user through interactions or by an events, such as successful AJAX call.
+
+// Read more on Actions - https://redux.js.org/docs/basics/Actions.html
+
 export function incrementNum(payload) {
   return {
     type: "INCREMENT_NUM",
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,3 +1,11 @@
+// Reducers receive information from Actions in the form of "Type" and "Payload".
+// And they perform a task or execute a fuction accordingly.
+
+// For example, on receiving action type of "INCREMENT_NUM",
+// the reducer function will increment the value stored in state, which is 0 initially.
+
+// Read more on Reducers - https://redux.js.org/docs/basics/Reducers.html
+
 export default function reducer(state = 0, action) {
   switch (action.type) {
     case "INCREMENT_NUM": 
